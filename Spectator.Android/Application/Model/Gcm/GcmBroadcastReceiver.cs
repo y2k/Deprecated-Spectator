6a8e094d406e56abed37f9f66fbca90003d94a13
y2k <itwisterlx@gmail.com>
2014-11-17 23:17:16 +0400

    android: GCM

---
+++ Spectator.Android/Application/Model/Gcm/GcmBroadcastReceiver.cs
@@ -0,0 +1,22 @@
+﻿using Android.Content;
+using Gcm.Client;
+
+namespace Spectator.Android.Application.Model.Gcm
+{
+	[BroadcastReceiver (Permission = Constants.PERMISSION_GCM_INTENTS)]
+	[IntentFilter (
+		new [] { Constants.INTENT_FROM_GCM_MESSAGE }, 
+		Categories = new [] { "@PACKAGE_NAME@" })]
+	[IntentFilter (
+		new [] { Constants.INTENT_FROM_GCM_REGISTRATION_CALLBACK }, 
+		Categories = new [] { "@PACKAGE_NAME@" })]
+	[IntentFilter (
+		new [] { Constants.INTENT_FROM_GCM_LIBRARY_RETRY },
+		Categories = new [] { "@PACKAGE_NAME@" })]
+	public class GcmBroadcastReceiver : GcmBroadcastReceiverBase<GcmService>
+	{
+		//IMPORTANT: Change this to your own Sender ID!
+		//The SENDER_ID is your Google API Console App Project Number
+		public static string[] SENDER_IDS = { "697360970929" };
+	}
+}
---
+++ Spectator.Android/Application/Model/Gcm/GcmService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Android.App;
+using Gcm.Client;
+using Android.Content;
+
+namespace Spectator.Android.Application.Model.Gcm
+{
+	[Service] //Must use the service tag
+	public class GcmService : GcmServiceBase
+	{
+		public GcmService () : base (GcmBroadcastReceiver.SENDER_IDS)
+		{
+		}
+
+		protected override void OnRegistered (Context context, string registrationId)
+		{
+			//Receive registration Id for sending GCM Push Notifications to
+		}
+
+		protected override void OnUnRegistered (Context context, string registrationId)
+		{
+			//Receive notice that the app no longer wants notifications
+		}
+
+		protected override void OnMessage (Context context, Intent intent)
+		{
+			//Push Notification arrived - print out the keys/values
+			if (intent == null || intent.Extras == null)
+				foreach (var key in intent.Extras.KeySet())
+					Console.WriteLine ("Key: {0}, Value: {1}", key, intent.Extras.GetString (key));
+		}
+
+		protected override bool OnRecoverableError (Context context, string errorId)
+		{
+			//Some recoverable error happened
+		}
+
+		protected override void OnError (Context context, string errorId)
+		{
+			//Some more serious error happened
+		}
+	}
+}
