78399e8a7f20aba27cb872b909d15c695216f466
y2k <itwisterlx@gmail.com>
2015-03-11 10:17:00 +0400

    refactoring

--- Spectator.Core/ViewModels/SubscriptionsViewModel.cs
+++ Spectator.Core/ViewModels/SubscriptionsViewModel.cs
@@ -1,18 +1,30 @@
 ﻿using GalaSoft.MvvmLight;
-using GalaSoft.MvvmLight.Command;
 using Spectator.Core.Model;
 using Spectator.Core.Model.Database;
 using Spectator.Core.ViewModels.Messages;
-using System;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Windows.Input;
 
 namespace Spectator.Core.ViewModels
 {
     public class SubscriptionsViewModel : ViewModelBase
     {
-        public ObservableCollection<SubscriptionItemViewModel> Subscriptions { get; } = new ObservableCollection<SubscriptionItemViewModel>();
+        public ObservableCollection<Subscription> Subscriptions { get; } = new ObservableCollection<Subscription>();
+
+        int _selectedItem;
+        public int SelectedItem
+        {
+            get { return _selectedItem; }
+            set
+            {
+                Set(ref _selectedItem, value);
+                SelectedSubscriptionChanged();
+            }
+        }
+
+        void SelectedSubscriptionChanged()
+        {
+            MessengerInstance.Send(new SelectSubscriptionMessage() { Id = Subscriptions[_selectedItem].Id });
+        }
 
         public SubscriptionsViewModel()
         {
@@ -23,21 +35,7 @@ namespace Spectator.Core.ViewModels
         {
             var model = new SubscriptionCollectionModel();
             await model.Reload();
-            Subscriptions.ReplaceAll((await model.Get()).Select(s => new SubscriptionItemViewModel(s)));
-        }
-
-        public class SubscriptionItemViewModel : ViewModelBase
-        {
-            public string Title { get; set; }
-
-            public RelayCommand SelectCommand { get; set; }
-
-            public SubscriptionItemViewModel(Subscription subscription)
-            {
-                Title = subscription?.Title;
-                SelectCommand = new SpectatorRelayCommand(() =>
-                    MessengerInstance.Send(new SelectSubscriptionMessage() { Id = subscription.Id }));
-            }
+            Subscriptions.ReplaceAll(await model.Get());
         }
     }
 }
